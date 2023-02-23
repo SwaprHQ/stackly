134049d92389f78daf7f35729b2c4fc353b96653
Adam Azad <adam@adamazad.com>
2023-02-23 22:59:14 +0000

    fix: ci issues

--- packages/sdk/src/utils/invariant/index.ts
+++
@@ -1,8 +0,0 @@
-const isProduction: boolean = process.env.NODE_ENV === 'production';
-
-export function enforce(
-  condition: boolean,
-  message: string
-): asserts condition {
-  if (!condition) throw new Error(isProduction ? undefined : message);
-}
