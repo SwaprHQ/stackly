790d359ffd7b9858638fdbeb317787edc910a811
Berteotti <berteotti2@gmail.com>
2023-05-12 11:46:45 +0100

    refactor: new order confirmation
    closes STK-57

--- packages/app/src/components/Modal/CreateVaultSteps.tsx
+++ packages/app/src/components/Modal/CreateVaultSteps.tsx
@@ -4,18 +4,19 @@ import {
   ModalBackdrop,
   ModalInnerWrapper,
   ModalHeader,
-  Step,
   ModalTitle,
   ModalContent as ModalContentBase,
 } from './styles';
 import { ContractReceipt, ContractTransaction } from 'ethers';
-import { getExplorerLink } from '../../utils';
 import { ChainId } from 'dca-sdk';
 import styled from 'styled-components';
+import { Check, X } from 'react-feather';
 
 export enum CreateVaultAndDepositStep {
   APPROVE_FACTORY,
+  REJECT_APPROVE_FACTORY,
   CREATE_ORDER,
+  REJECT_CREATE_ORDER,
 }
 
 export type VaultCreateAndDepositStepsModalProps = {
@@ -44,28 +45,27 @@ export function CreateVaultStepsModal() {
     throw new Error('No data provided to modal. Expected vault data.');
   }
 
-  const {
-    approveFactoryTransaction,
-    approveFactoryReceipt,
-    createOrderTransaction,
-    createOrderReceipt,
-    stepsCompleted,
-    chainId,
-  } = data;
+  const { createOrderReceipt, stepsCompleted } = data;
 
-  const isFactoryApproved =
-    approveFactoryReceipt && stepsCompleted.includes(CreateVaultAndDepositStep.APPROVE_FACTORY) ? true : false;
+  const isApprovingFactory = stepsCompleted.includes(CreateVaultAndDepositStep.APPROVE_FACTORY);
 
-  const isOrderCreated =
-    createOrderReceipt && stepsCompleted.includes(CreateVaultAndDepositStep.CREATE_ORDER) ? true : false;
+  const isFactoryApprovedRejected = stepsCompleted.includes(CreateVaultAndDepositStep.REJECT_APPROVE_FACTORY);
 
-  const approveFactoryTransactionLink = approveFactoryTransaction
-    ? getExplorerLink(chainId, approveFactoryTransaction.hash, 'transaction')
-    : undefined;
+  const isOrderRejected = stepsCompleted.includes(CreateVaultAndDepositStep.REJECT_CREATE_ORDER);
 
-  const createOrderTransactionLink = createOrderTransaction
-    ? getExplorerLink(chainId, createOrderTransaction.hash, 'transaction')
-    : undefined;
+  const isCreateingOrder = !createOrderReceipt && stepsCompleted.includes(CreateVaultAndDepositStep.CREATE_ORDER);
+
+  const isOrderCreated = createOrderReceipt && stepsCompleted.includes(CreateVaultAndDepositStep.CREATE_ORDER);
+
+  // This can be used in the future
+
+  // const approveFactoryTransactionLink = approveFactoryTransaction
+  //   ? getExplorerLink(chainId, approveFactoryTransaction.hash, 'transaction')
+  //   : undefined;
+
+  // const createOrderTransactionLink = createOrderTransaction
+  //   ? getExplorerLink(chainId, createOrderTransaction.hash, 'transaction')
+  //   : undefined;
 
   return (
     <ModalBackdrop onClick={closeModal}>
@@ -75,27 +75,39 @@ export function CreateVaultStepsModal() {
             <ModalTitle>Create Order</ModalTitle>
           </ModalHeader>
           <ModalContent>
-            {isFactoryApproved ? (
-              <Step href={approveFactoryTransactionLink} isSuccess={true} target="_blank" rel="noopener noreferrer">
-                Approved Factory
-              </Step>
-            ) : approveFactoryTransaction ? (
-              <Step href={approveFactoryTransactionLink} isBusy={true} target="_blank" rel="noopener noreferrer">
-                Approving Factory
-              </Step>
-            ) : (
-              <Step isBusy={true}>Approving Factory</Step>
+            {isApprovingFactory && (
+              <Message>
+                <Text>Approving Factory ...</Text>
+              </Message>
             )}
-            {isOrderCreated ? (
-              <Step href={createOrderTransactionLink} isSuccess={true} target="_blank" rel="noopener noreferrer">
-                Created Order
-              </Step>
-            ) : createOrderTransaction ? (
-              <Step href={createOrderTransactionLink} isBusy={true} target="_blank" rel="noopener noreferrer">
-                Creating Order
-              </Step>
-            ) : (
-              <Step>Create Order</Step>
+            {isFactoryApprovedRejected && (
+              <Message>
+                <RejectedCircle>
+                  <X />
+                </RejectedCircle>
+                <RejectedText>Approve Factory Failed</RejectedText>
+              </Message>
+            )}
+            {isCreateingOrder && (
+              <Message>
+                <Text>Creating Order ...</Text>
+              </Message>
+            )}
+            {isOrderCreated && (
+              <Message>
+                <ApprovedCircle>
+                  <Check />
+                </ApprovedCircle>
+                <Text>Order Created!</Text>
+              </Message>
+            )}
+            {isOrderRejected && (
+              <Message>
+                <RejectedCircle>
+                  <X />
+                </RejectedCircle>
+                <RejectedText>Create Order Failed</RejectedText>
+              </Message>
             )}
           </ModalContent>
         </ModalInnerWrapper>
@@ -103,6 +115,48 @@ export function CreateVaultStepsModal() {
     </ModalBackdrop>
   );
 }
+
+const Text = styled.p`
+  font-weight: 700;
+  font-size: 20px;
+`;
+
+const RejectedText = styled(Text)`
+  color: #ed3131;
+`;
+
+const ApprovedCircle = styled.div`
+  background-color: #000;
+  border-radius: 50px;
+  height: 35px;
+  width: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
+
+const RejectedCircle = styled.div`
+  background-color: #ed3131;
+  border-radius: 50px;
+  height: 35px;
+  width: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+`;
+
+const Message = styled.div`
+  margin: 28px auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  & > *:not(:last-child) {
+    margin-bottom: 22px;
+  }
+`;
 
 const ModalContent = styled(ModalContentBase)`
   padding-top: 0;
