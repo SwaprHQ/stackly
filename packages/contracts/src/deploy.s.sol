f868049862d4d2128bbb43529491d882e35e996e
Nelson Galdeman <nelsongaldeman@gmail.com>
2023-07-03 13:11:04 +0100

    feat: foundry deploy scripts

---
+++ packages/contracts/src/deploy.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "./DCAOrder.sol";
+import "./OrderFactory.sol";
+
+contract Deploy is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        new DCAOrder();
+        new OrderFactory();
+
+        vm.stopBroadcast();
+    }
+}
