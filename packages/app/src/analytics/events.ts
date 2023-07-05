f34e15548a46d95c364ca70f01bc73288c4cfd42
Leonardo Berteotti <berteotti2@gmail.com>
2023-07-05 21:03:04 +0100

    feat: add discord button event to env (#71)

--- packages/app/src/analytics/events.ts
+++ packages/app/src/analytics/events.ts
@@ -1,5 +1,5 @@
 export const DISCORD_BUTTON_CLICK = 'click/discord-button-link';
 
 export const events: Record<string, string> = {
-  [DISCORD_BUTTON_CLICK]: 'F9PDRRNP',
+  [DISCORD_BUTTON_CLICK]: process.env.REACT_APP_DISCORD_BUTTON_CLICK_EVENT as string,
 };
