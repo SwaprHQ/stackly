e7d3d78ef2e66113e33a47100dac7b1fed3aff0f
Nelson Galdeman <nelsongaldeman@gmail.com>
2023-07-11 17:43:00 +0100

    refactor: adds feeAmount (nominal) and fee (%) to subgraph DCAOrder entity

--- packages/subgraph/src/mappings/order.ts
+++ packages/subgraph/src/mappings/order.ts
@@ -1,7 +1,10 @@
-import { Address } from '@graphprotocol/graph-ts';
+import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { Token, DCAOrder } from '../../generated/schema';
 import { ERC20 as ERC20Contract } from '../../generated/templates/DCAOrder/ERC20';
 import { Initialized, Cancelled, DCAOrder as DCAOrderContract } from '../../generated/templates/DCAOrder/DCAOrder';
+import { OrderFactory } from '../../generated/OrderFactory/OrderFactory';
+
+const HUNDRED_PERCENT = BigInt.fromI32(10000);
 
 export function createOrReturnTokenEntity(contractAddress: Address): Token {
   // Persist token data if it doesn't already exist
@@ -26,7 +29,19 @@ export function handleDCAOrderInitialized(event: Initialized): void {
   order.sellToken = createOrReturnTokenEntity(orderContract.sellToken()).id;
   order.buyToken = createOrReturnTokenEntity(orderContract.buyToken()).id;
   order.receiver = orderContract.receiver();
+
+  let protocolFee : BigInt = BigInt.fromI32(0);
+  let orderFactoryAddress: Address = (event.transaction.to !== null ? event.transaction.to : Address.fromString("0x0"))!;
+  
+  const factory = OrderFactory.bind(orderFactoryAddress);
+  let tryProtocolFee = factory.try_protocolFee();
+  if (!tryProtocolFee.reverted) {
+    protocolFee = BigInt.fromI32(tryProtocolFee.value);
+  }
+
   order.amount = orderContract.amount();
+  order.fee = protocolFee;
+  order.feeAmount = (order.amount.times(protocolFee)).div(HUNDRED_PERCENT.minus(protocolFee));
   order.endTime = orderContract.endTime().toI32();
   order.startTime = orderContract.startTime().toI32();
   order.orderSlots = orderContract.orderSlots();
