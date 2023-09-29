12dad13bb80bab74c2acfdce4151ece3512cae9f
Nelson Galdeman <nelsongaldeman@gmail.com>
2023-09-29 13:51:06 +0100

    chore: ethereum deployment

--- packages/contracts/src/deploy.s.sol
+++ packages/contracts/src/deploy.s.sol
@@ -6,13 +6,12 @@ import "./DCAOrder.sol";
 import "./OrderFactory.sol";
 
 contract Deploy is Script {
-    address whitelistNFT = vm.envAddress("WHITELIST_NFT_ADDRESS");
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         new DCAOrder();
-        new OrderFactory(whitelistNFT);
+        new OrderFactory();
 
         vm.stopBroadcast();
     }
